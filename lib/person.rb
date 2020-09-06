a41ad482b4a9e7c5ae00681bfa481e9dc0cf454b
brynknowles <bryn.knowles@gmail.com>
2020-09-05 23:07:40 -0400

    completed lab

--- lib/person.rb
+++ lib/person.rb
@@ -35,6 +35,41 @@ class Person
         hygiene > 7
     end
 
+    def get_paid(salary)
+        self.bank_account += salary
+        self.happiness += 1
+        "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        '♪ Rub-a-dub just relaxing in the tub ♫'
+    end
+
+    def work_out
+        self.hygiene -= 3
+        self.happiness += 2
+        '♪ another one bites the dust ♫'
+    end
+
+    def call_friend(friend)
+        [self, friend].each {|o|o.happiness += 3}
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(person, topic)
+        speakers = [self, person]
+        if topic == "politics"
+            speakers.each {|s| s.happiness -= 2}
+            "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            speakers.each {|s| s.happiness += 1}
+            "blah blah sun blah rain"
+        else
+            "blah blah blah blah blah"
+        end
+    end
+
     def self.all
         @@all
     end
